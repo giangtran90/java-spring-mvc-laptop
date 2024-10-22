8c92e919af5f8851892c04dfc5dcaeca255dbdbc
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-10-22 14:36:20 +0900

    Thực hiện hiển thị chi tiết một sản phẩm (OK)

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -5,11 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
 
+	private final ProductService productService;
+	
+	public ItemController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
 	@GetMapping("/products/{id}")
-	public String getProductDetail(Model model,@PathVariable long id) {
+	public String getProductDetail(Model model, @PathVariable long id) {
+		Product product = this.productService.fetchProductById(id);
+		model.addAttribute("product", product);
 		return "/client/product/detail";
 	}
 }
--- src/main/webapp/WEB-INF/view/client/homepage/show.jsp
+++ src/main/webapp/WEB-INF/view/client/homepage/show.jsp
@@ -126,7 +126,7 @@
 													style="top: 10px; left: 10px;">Laptop</div>
 												<div
 													class="p-4 border border-secondary border-top-0 rounded-bottom">
-													<h4 style="font-size: 15px">${product.name}</h4>
+													<h4 style="font-size: 15px"><a href="/products/${product.id}">${product.name}</a></h4>
 													<p style="font-size: 13px">${product.shortDesc}</p>
 													<div class="d-flex flex-lg-wrap">
 														<p style="font-size: 15px; text-align: center; width: 100%" class="text-dark fw-bold mb-3"><fmt:formatNumber type="number" value="${product.price}" /> đ</p>
