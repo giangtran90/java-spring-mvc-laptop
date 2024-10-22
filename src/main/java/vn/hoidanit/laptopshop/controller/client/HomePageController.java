d059dbb3d804452d167ef437a8870eb17cc73b4a
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-10-22 16:55:37 +0900

    Thực hiện mapper đơn giản để lưu account vào DB + Sửa lại Validate dữ liệu ở User cho chính xác + sau khi lưu chuyển sang page login

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -2,6 +2,7 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +10,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class HomePageController {
 
 	private final ProductService productService;
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
 	
-	public HomePageController(ProductService productService) {
+	public HomePageController(ProductService productService, UserService userService, PasswordEncoder passwordEncoder) {
 		super();
 		this.productService = productService;
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
 	}
 
+	// All product
 	@GetMapping("/")
 	public String getHomePage(Model model) {
 		List<Product> products = this.productService.fetchAllProduct();
@@ -29,14 +37,30 @@ public class HomePageController {
 		return "/client/homepage/show";
 	}
 	
+	// Get Register page
 	@GetMapping("/register")
 	public String getRegisterPage(Model model) {
 		model.addAttribute("registerUser", new RegisterDTO());
 		return "/client/auth/register";
 	}
 	
+	// Create a Register
 	@PostMapping("/register")
-	public String createAcount(@ModelAttribute("registerUser") RegisterDTO acount) {
-		return "redirect:/client/auth/register";
+	public String createAcount(@ModelAttribute("registerUser") RegisterDTO registerDTO) {;
+		
+		User user = this.userService.RegisterDTOtoUser(registerDTO);
+		String hashPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(hashPassword);
+		user.setRole(this.userService.fetchRoleByName("USER"));
+		// save
+		userService.handleSaveUser(user);
+		return "redirect:/login";
 	}
+	
+	// Get Login page
+	@GetMapping("/login")
+	public String getLoginPage(Model model) {
+		return "/client/auth/login";
+	}
+	
 }
